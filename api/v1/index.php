56e0a5c56ad210db080b84dd58638f6c7e54ad6c
SkipperStrange <skipperstrange@gmail.com>
2021-11-21 04:16:32 +0000

    initial commit. Front end app/ + api

--- api/v1/index.php
+++ api/v1/index.php
@@ -1,15 +1,7 @@
 <?php
 require '../../bootstrap-api.php';
 
-
-use Premium\Middleware\FileFilter as FileFilter;
-use Premium\Middleware\ImageRemoveExif as ImageRemoveExif;
-use Premium\Middleware\ImageFileMove as ImageFileMove;
 use Premium\Middleware\Logging as Log;
-
-$filter               = new FileFilter();
-$removeExif           = new ImageRemoveExif();
-$listingImageFileMove = new ImageFileMove();
 
 
 $app->add(new Log());
